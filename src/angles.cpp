144712ec656d81e162c96610613b05860e86a451
Dezsö Babai <dezso.babai@tum.de>
2024-03-18 23:33:38 +0100

    fix random chi angles

--- src/angles.cpp
+++ src/angles.cpp
@@ -1,13 +1,12 @@
 #include "angles.h"
+#include "spdlog/spdlog.h"
 
-
-#include <iostream>
 #include <random>
 #include <vector>
 
 //https://dunbrack.fccc.edu/bbdep2010/ConformationalAnalysis.php
 double chi1() {
-
+    auto logger = spdlog::get("main");
     // Seed the Mersenne Twister engine
     static std::random_device rd;
     static std::mt19937 mt(rd());
@@ -15,7 +14,6 @@ double chi1() {
     // Define parameters for the normal distributions
     const std::vector<double> means = {120.0, 240.0, 360.0};
     const double stddev = 2.0;
-    const std::vector<double> weights = {1/3, 1/3, 1/3};
 
     // Create the normal distributions
     std::vector<std::normal_distribution<double>> dists;
@@ -24,16 +22,11 @@ double chi1() {
     }
 
     // Generate random numbers from the mixture
-    double randomValue = mt() / static_cast<double>(mt.max()); // normalize to [0, 1]
-    double cumulativeWeight = 0.0;
+    std::uniform_int_distribution<> choicedist(0, 2);
 
-    for (size_t i = 0; i < means.size(); ++i) {
-        cumulativeWeight += weights[i];
-        if (randomValue < cumulativeWeight) {
-            return dists[i](mt);
-        }
-    }
-    // This line should not be reached under normal circumstances
-    return dists.back()(mt);
+    int choice = choicedist(mt);
+    double ang = dists[choice](mt);
+    logger->info(std::format("Chosen chi1 angle: {} degrees.", ang));
+    return ang;
 }
 
